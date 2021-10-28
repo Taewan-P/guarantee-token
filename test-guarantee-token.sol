8f8adb049632e2302582b0eb0fcb7c53c4a9567f
Taewan Park <swimingtw@gmail.com>
2021-10-28 15:58:47 +0900

    Update token to be mintable
    Co-Authored-By: JaeHun Lee <yeolkeut@gmail.com>

--- test-guarantee-token.sol
+++ test-guarantee-token.sol
@@ -1,7 +1,19 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
 
-import "openzeppelin-standard/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract GuaranteeToken is ERC721 {
+contract GuaranteeToken is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
     constructor() ERC721("GuaranteeToken", "GTK") {}
+
+    function safeMint(address to) public onlyOwner {
+        _safeMint(to, _tokenIdCounter.current());
+        _tokenIdCounter.increment();
+    }
 }
