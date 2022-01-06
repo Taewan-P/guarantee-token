0165c543dfa754f7c1c6e0556ae14b7ed54c210b
Taewan Park <swimingtw@gmail.com>
2022-01-06 18:05:03 +0900

    Update token to be enumerable
    Co-Authored-By: JaeHun Lee <yeolkeut@gmail.com>

--- test-guarantee-token.sol
+++ test-guarantee-token.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract GuaranteeToken is ERC721, Ownable {
+contract GuaranteeToken is ERC721, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -15,5 +16,21 @@ contract GuaranteeToken is ERC721, Ownable {
     function safeMint(address to) public onlyOwner {
         _safeMint(to, _tokenIdCounter.current());
         _tokenIdCounter.increment();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
