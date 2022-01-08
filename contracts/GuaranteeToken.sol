98ed8bc81946d5e73479bb6607af8fee10c37eb3
Taewan Park <swimingtw@gmail.com>
2022-01-09 01:04:44 +0900

    Add token and deployment code
    Co-Authored-By: JaeHun Lee <yeolkeut@gmail.com>

---
+++ contracts/GuaranteeToken.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract GuaranteeToken is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("GuaranteeToken", "GTK") {}
+
+    function safeMint(address to) public onlyOwner {
+        _safeMint(to, _tokenIdCounter.current());
+        _tokenIdCounter.increment();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ migrations/2_deploy_gtktoken.js
@@ -0,0 +1,5 @@
+var Guarantee = artifacts.require("GuaranteeToken");
+
+module.exports = function(deployer) {
+  deployer.deploy(Guarantee);
+};
