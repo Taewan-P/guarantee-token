e3b65fd61b10fe770fc2c4c99057adfbd3146043
Taewan Park <swimingtw@gmail.com>
2022-01-19 18:21:13 +0900

    Update contract to have minting roles

--- contracts/GuaranteeToken.sol
+++ contracts/GuaranteeToken.sol
@@ -3,20 +3,26 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract GuaranteeToken is ERC721, ERC721Enumerable, Ownable {
+contract GuaranteeToken is ERC721, ERC721Enumerable, AccessControl {
     using Counters for Counters.Counter;
 
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("GuaranteeToken", "GTK") {}
-
-    function safeMint(address to) public onlyOwner {
-        _safeMint(to, _tokenIdCounter.current());
-        _tokenIdCounter.increment();
+    constructor() ERC721("GuaranteeToken", "GTK") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
+
+    function safeMint(address to) public onlyRole(MINTER_ROLE) {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
@@ -28,7 +34,7 @@ contract GuaranteeToken is ERC721, ERC721Enumerable, Ownable {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable)
+        override(ERC721, ERC721Enumerable, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
