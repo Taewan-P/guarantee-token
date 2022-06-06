07ff1b0d8a17bb57409766e8621889120d24bdb3
Taewan Park <swimingtw@gmail.com>
2022-06-06 14:45:37 +0900

    Set migration script to use env vars

--- migrations/2_deploy_gtktoken.js
+++ migrations/2_deploy_gtktoken.js
@@ -1,5 +1,17 @@
 var Guarantee = artifacts.require("GuaranteeToken");
 
-module.exports = function(deployer) {
+module.exports = (deployer, network, addresses) => {
+  const Web3 = require('web3');
+  const TruffleConfig = require('../truffle-config.js');
+
+  const config = TruffleConfig.networks[network];
+
+  if (process.env.ACCOUNT_PASSWORD) {
+    var web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
+
+    console.log('>> Unlocking account ' + config.from);
+    web3.eth.personal.unlockAccount(config.from, process.env.ACCOUNT_PASSWORD, 36000);
+  }
+  
   deployer.deploy(Guarantee);
 };
